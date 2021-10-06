8a20857000d3549969bf132d789571862809161b
tonocp <amcp88@gmail.com>
2021-10-06 07:58:38 +0200

    MEAN CRUD SuperHeroes APP

--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
 import { Heroe } from '../interfaces/heroes.interface';
-import { environment } from '../../../environments/environment.prod';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
