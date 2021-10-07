18482ce0c95e3ec29b4b64b47493a48fd2ba5ed4
tonocp <amcp88@gmail.com>
2021-10-07 07:26:40 +0200

    MEAN CRUD SuperHeroes APP

--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -14,35 +14,38 @@ export class HeroesService {
   constructor(private http: HttpClient) {}
 
   getHeroes(): Observable<Heroe[]> {
-    return this.http.get<Heroe[]>(`${this.baseUrl}/heroes/list`);
+    return this.http.get<Heroe[]>(`${this.baseUrl}/api/heroes/list`);
   }
 
   getHeroePorId(id: string): Observable<Heroe> {
-    return this.http.get<Heroe>(`${this.baseUrl}/heroes/${id}`, {
+    return this.http.get<Heroe>(`${this.baseUrl}/api/heroes/${id}`, {
       headers: { id: id },
     });
   }
 
   getSugerencias(termino: string): Observable<Heroe[]> {
-    return this.http.get<Heroe[]>(`${this.baseUrl}/heroes/search/${termino}`, {
-      headers: { termino: termino },
-    });
+    return this.http.get<Heroe[]>(
+      `${this.baseUrl}/api/heroes/search/${termino}`,
+      {
+        headers: { termino: termino },
+      }
+    );
   }
 
   agregarHeroe(heroe: Heroe): Observable<Heroe> {
     if (heroe.alt_img === '') heroe.alt_img = 'assets/no-image.png';
-    return this.http.post<Heroe>(`${this.baseUrl}/heroes/new`, heroe);
+    return this.http.post<Heroe>(`${this.baseUrl}/api/heroes/new`, heroe);
   }
 
   actualizarHeroe(heroe: Heroe): Observable<Heroe> {
     return this.http.put<Heroe>(
-      `${this.baseUrl}/heroes/edit/${heroe.id}`,
+      `${this.baseUrl}/api/heroes/edit/${heroe.id}`,
       heroe
     );
   }
 
   borrarHeroe(_id: string): Observable<any> {
-    return this.http.delete<any>(`${this.baseUrl}/heroes/${_id}`, {
+    return this.http.delete<any>(`${this.baseUrl}/api/heroes/${_id}`, {
       headers: { _id: _id },
     });
   }
